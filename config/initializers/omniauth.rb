2137ad6f857c8eb8e654e4ab087eeaf296fd6e0e
dark violet <fabrice.passaquay@gmail.com>
2016-09-05 18:21:24 +0200

    it works!

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '288263018221730', '58074b86e305233d973ec5d619109763'
+end
