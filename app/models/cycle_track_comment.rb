30f1faf9d98e6d21fa258a235aa3644c7196fd01
dark violet <fabrice.passaquay@gmail.com>
2016-10-11 16:29:06 +0200

    streetview images added, most commented added

--- app/models/cycle_track_comment.rb
+++ app/models/cycle_track_comment.rb
@@ -2,6 +2,7 @@ class CycleTrackComment < ApplicationRecord
   belongs_to :User
   belongs_to :CycleTrack
 
+scope :most_commented, -> (num) { CycleTrackComment.group('cycle_track_id').order('count_id desc').limit(num).count('id')}
 
 
 end
