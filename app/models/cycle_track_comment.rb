5247c24dad82709ab1eb00ab87f7c7c242b468b5
dark violet <fabrice.passaquay@gmail.com>
2016-10-20 17:20:28 +0200

    top contributeurs ajouté

--- app/models/cycle_track_comment.rb
+++ app/models/cycle_track_comment.rb
@@ -4,5 +4,6 @@ class CycleTrackComment < ApplicationRecord
 
 scope :most_commented, -> (num, othernum) { CycleTrackComment.where(id_site: othernum).group('cycle_track_id').order('count_id desc').limit(num).count('id')}
 
+scope :top_users, -> (num, othernum) { CycleTrackComment.where(id_site: othernum).group('users_id').order('count_users_id desc').limit(num).count('users_id')}
 
 end
